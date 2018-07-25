a2a32f66f41133d9dc8da61b576e9c4268c12356
ulv0710 <ulv0710@gmail.com>
2018-07-25 19:52:21 +0300

    SkyBox class

--- oglProject/SkyBox.cpp
+++ oglProject/SkyBox.cpp
@@ -16,6 +16,21 @@ SkyBox::SkyBox(
 	faces[4] = positiveZ;   faces[5] = negativeZ;
 
 	cubemapTexture = loadCubemap(faces);
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, 72, vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 36, indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0);
 }
 
 void SkyBox::DrawSky(
